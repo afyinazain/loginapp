4fcba00ed4cb7daf058f32c69de04df6d8327750
PhoenyixDev <afyina9141@gmail.com>
2026-02-27 12:28:10 +0800

    Update _My Profile.py

--- pages/_My Profile.py
+++ pages/_My Profile.py
@@ -330,17 +330,16 @@ if pending_payment.empty:
 
 for _, row in pending_payment.iterrows():
     
-    with st.expander(
-        f"{row['item_1']} {row['bil_jam']} | {row['nama_tempat']} | {row['delivery_date']}"
-    ):
+    with st.expander(st.markdown(f"<span style='color:red;'>Balance To Be Paid: RM {float(row['lookup_pivot3']):.2f}</span>",unsafe_allow_html=True):
+        st.write(f"{row['item_1']} {row['bil_jam']} | {row['nama_tempat']} | {row['delivery_date']}")
         st.write(f"Branch: {row['branch']}")
         st.write(f"Invoice: {row['invoice_num']}")
         st.write(f"Total: RM {float(row['total']):.2f}")
-        
         st.markdown(f"<span style='color:red;'>Balance To Be Paid: RM {float(row['lookup_pivot3']):.2f}</span>",unsafe_allow_html=True)
         st.markdown(f'<a href="{row["wa_link"]}" target="_blank">📲 WhatsApp</a>',unsafe_allow_html=True)
 
     
+
 
 
 
