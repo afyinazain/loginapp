394048ba141311e2514e0b3a2f466364d5b6848d
PhoenyixDev <afyina9141@gmail.com>
2026-02-27 12:19:09 +0800

    Update _My Profile.py

--- pages/_My Profile.py
+++ pages/_My Profile.py
@@ -68,6 +68,12 @@ invoices = df_inv[
     (df_inv["lookup_pivot3"] > 0)
 ]
 
+pending_payment = df_inv[
+    (df_inv["salesperson"] == user_name) &
+    (df_inv["type_status"] == "Invoice") &
+    (df_inv["lookup_pivot3"] > 0) &
+    (df_inv["delivery_date"] > datetime.today()
+]
 
 
 # ----------------------------
@@ -330,6 +336,7 @@ for _, row in invoices.iterrows():
         st.markdown(f'<a href="{row["wa_link"]}" target="_blank">📲 WhatsApp</a>',unsafe_allow_html=True)
 
     
+
 
 
 
